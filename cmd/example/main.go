215a7323466a61f9eae4fadd77500b28d680349e
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-03-19 17:31:16 +0200

    added using of compute handler in main file

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"flag"
+	"io"
+	lab2 "kpi-lab2"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 var (
@@ -21,6 +25,35 @@ func main() {
 
 	if *inputExpression == "" && *inputFile == "" {
 		_, _ = os.Stderr.WriteString("You must use one of these arguments: -e, -f\n")
+		os.Exit(1)
+	}
+
+	var reader io.Reader
+	var writer io.Writer
+
+	if *inputExpression != "" {
+		reader = strings.NewReader(*inputExpression)
+	}
+
+	if *inputFile != "" {
+		file, _ := os.OpenFile(filepath.Join(".", *inputFile), os.O_RDONLY, 0644)
+		defer file.Close()
+		reader = file
+	}
+
+	if *outputFile != "" {
+		writer, _ = os.Create(filepath.Join(".", *outputFile))
+	} else {
+		writer = os.Stdout
+	}
+
+	handler := &lab2.ComputeHandler{
+		Input:  reader,
+		Output: writer,
+	}
+
+	err := handler.Compute()
+	if err != nil {
 		os.Exit(1)
 	}
 }
